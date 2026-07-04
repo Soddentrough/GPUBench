9167316d2ac7a7f7ad5d19be208b4c1bc5248637
Soddentrough <m.j.n.rogers@gmail.com>
2026-07-04 22:00:30 +0900

    Fix OpenCL header inclusion on macOS by using OpenCL/opencl.h framework header

--- cpp_src/core/OpenCLContext.h
+++ cpp_src/core/OpenCLContext.h
@@ -3,7 +3,11 @@
 #include "IComputeContext.h"
 #define CL_TARGET_OPENCL_VERSION 300
 #include "utils/DynamicLibrary.h"
+#ifdef __APPLE__
+#include <OpenCL/opencl.h>
+#else
 #include <CL/cl.h>
+#endif
 #include <memory>
 #include <stdexcept>
 #include <string>
